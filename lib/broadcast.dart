b65b365e7e7adff1108e7ace855e86334040931d
h.enoki <h.enoki@stv-tech.co.jp>
2021-03-26 18:25:25 +0900

    commit test

--- lib/broadcast.dart
+++ lib/broadcast.dart
@@ -2,22 +2,30 @@ import 'package:beacon_broadcast/beacon_broadcast.dart';
 
 class Broadcast {
   BeaconBroadcast beaconBroadcast = BeaconBroadcast();
-  Future<void> broadcastOn() async {
-    print("BroadcastOn");
-    bool isAdvertising = await beaconBroadcast.isAdvertising();
-    BeaconStatus transmissionSupportStatus =
-        await beaconBroadcast.checkTransmissionSupported();
+  // Future<void> broadcastOn() async {
+  //   print("BroadcastOn");
+  //   bool isAdvertising = await beaconBroadcast.isAdvertising();
+  //   BeaconStatus transmissionSupportStatus =
+  //       await beaconBroadcast.checkTransmissionSupported();
+  //
+  //   if (!isAdvertising &&
+  //       (transmissionSupportStatus == BeaconStatus.supported)) {
+  //     print("*****");
+  //     await beaconBroadcast
+  //         .setUUID("00008030-00054D821AFB802E")
+  //         .setMajorId(1)
+  //         .setMinorId(100)
+  //         .setIdentifier('com.beacon.sample')
+  //         .start();
+  //   }
+  // }
 
-    if (!isAdvertising &&
-        (transmissionSupportStatus == BeaconStatus.supported)) {
-      print("*****");
-      await beaconBroadcast
-          .setUUID("00008030-00054D821AFB802E")
-          .setMajorId(1)
-          .setMinorId(100)
-          .setIdentifier('com.beacon.sample')
-          .start();
-    }
+  void broadcastOn() {
+    beaconBroadcast
+        .setUUID("00008030-00054D821AFB802E")
+        .setMajorId(1)
+        .setMinorId(100)
+        .start();
   }
 
   void broadcastOff() {
